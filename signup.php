8262274af8f1ddc42333f927ff6c41f32c729973
martian58 <fuadelizade6@gmail.com>
2024-01-14 17:35:50 +0400

    first commit

---
+++ signup.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>helo my name is yaaaaa</p>
+</body>
+</html>
