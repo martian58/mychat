d6b4b842b3e36796539a56a3b6bf1223d12e12f2
martian58 <fuadelizade6@gmail.com>
2024-01-17 00:38:08 +0400

    last changes

--- pages/contacts.php
+++ pages/contacts.php
@@ -13,17 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/home.css">
-    <style>
-        .message-div{
-            border-radius: 0px 5px 0px 5px;
-            height: auto;
-            width: 200px;
-            background-color: rgb(20, 71, 115);
-            color: white; 
-            margin-top: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/contacts.css">
     <title>Home</title>
 </head>
 <body>
@@ -33,8 +23,8 @@
     <div class="container">
         <div class="navbar">
             <ul>
-                <li>Home</li>
-                <li><a href="contacts.php">Contacts</a></li>
+                <li><a href="home.php">Home</a></li>
+                <li>Contacts</li>
                 <li id="logout"><a href="logout.php">Logout</a></li>
             </ul>
         </div>
@@ -49,28 +39,22 @@
                 $reciever_usr_name = "Fuad";
                 include '../api.php';
             
-                $sql = "select message from messages where sender_usr_name='$username' and reciever_usr_name='$reciever_usr_name';";
+                $sql = "select username from users;";
                 $result = mysqli_query($connect, $sql);
                 if($result){
                     $num = mysqli_num_rows($result);
                     if($num > 0){
                         while ($row = mysqli_fetch_assoc($result)) {
-                            echo '<div class="message-div" >' .$username .'<br>' . $row['message'] . '</div>';
+                            echo '<div id="contact-usernames">' .$row['username'] . '</div>';
                         }
                 
                     }
                 }
                 ?>
             </div>
-            <div class="message-area">
-                <form action="../php/send_msg.php" method="post">
-                    <input type="text" name="message_input" id="message_input" class="message_input">
-                    <input type="submit" value="Send">
-                </form>
-            </div>
         </div>
     </div>
-    <script src="../js/home.js"></script>
+    <script src="../js/contacts.js"></script>
     <script>
         scrollToBottom();
     </script>
